62d691ca69d426f5b43ccc1ad14499db0fda2ee4
Paul Tikken <paul.tikken@gmail.com>
2023-09-02 19:42:52 +0000

    altered setup.py to facilitate modular requirements installation as well as an 'all' option

--- setup.py
+++ setup.py
@@ -1,39 +1,116 @@
+#!/usr/bin/env python3
+import codecs
 import os
+from itertools import chain
 
 from setuptools import setup, find_packages
 
 from nldcsc import _version_from_git_describe
 
-# The directory containing this file
-HERE = os.path.abspath(os.path.dirname(__file__))
+NAME = "nldcsc"
 
-# The text of the README file
-with open(os.path.join(HERE, "README.md")) as fid:
-    README = fid.read()
+# -*- Extras -*-
 
-with open(os.path.join(HERE, "requirements.txt")) as fid:
-    REQS = fid.read().splitlines()
+MODULES = {
+    "loggers",
+}
+
+# -*- Requirements -*-
+
+
+def _strip_comments(l):
+    return l.split("#", 1)[0].strip()
+
+
+def _pip_requirement(req):
+    if req.startswith("-r "):
+        _, path = req.split()
+        return reqs(*path.split("/"))
+    return [req]
+
+
+def _reqs(*f):
+    return [
+        _pip_requirement(r)
+        for r in (
+            _strip_comments(l)
+            for l in open(os.path.join(os.getcwd(), "requirements", *f)).readlines()
+        )
+        if r
+    ]
+
+
+def reqs(*f):
+    """Parse requirement file.
+
+    Example:
+        reqs('default.txt')          # requirements/default.txt
+        reqs('modules', 'redis.txt')  # requirements/modules/redis.txt
+    Returns:
+        List[str]: list of requirements specified in the file.
+    """
+    return [req for subreq in _reqs(*f) for req in subreq]
+
+
+def extras(*p):
+    """Parse requirement in the requirements/modules/ directory."""
+    return reqs("modules", *p)
+
+
+def install_requires():
+    """Get list of requirements required for installation."""
+    return reqs("default.txt")
+
+
+def extras_require():
+    """Get map of all extra requirements."""
+    module_requirements = {x: extras(x + ".txt") for x in MODULES}
+
+    # add an 'all' value to install all requirements for all modules
+    module_requirements["all"] = list(set(chain(*module_requirements.values())))
+
+    return module_requirements
+
+
+# -*- Long Description -*-
+
+
+def long_description():
+    try:
+        return codecs.open("README.md", "r", "utf-8").read()
+    except OSError:
+        return "Long description error: Missing README.rst file"
 
 
 setup(
-    name="nldcsc",
+    name=NAME,
+    packages=find_packages(exclude=["tests", "test_data"]),
     version=_version_from_git_describe(),
-    packages=find_packages(exclude=("tests", "test_data")),
-    url="",
-    license="GNU General Public License v3.0",
-    author="NLDCSC",
     description="Package with general devops code",
-    long_description=README,
-    long_description_content_type="text/markdown",
+    long_description=long_description(),
+    author="NLDCSC",
+    author_email="NLDCSC@invalid.com",
+    url="https://github.com/NLDCSC/nldcsc",
+    license="GNU General Public License v3.0",
+    platforms=["any"],
+    install_requires=install_requires(),
+    extras_require=extras_require(),
+    python_requires=">=3.8",
     package_data={"nldcsc": ["LICENSE", "VERSION"]},
     include_package_data=True,
+    project_urls={
+        "Code": "https://github.com/NLDCSC/nldcsc",
+    },
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: POSIX :: Linux",
+        "Framework :: NLDCSC Devops",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
     ],
-    python_requires=">=3.10",
-    install_requires=REQS,
 )
