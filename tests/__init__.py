294aa4e14d9640c6171ad9aa68b8fe37723616e9
Paul Tikken <paul.tikken@gmail.com>
2023-09-05 15:29:44 +0000

    black formatting

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,12 @@
 import os
 
 if not os.path.exists(
-        os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "test_data")
+    os.path.join(
+        os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "test_data"
+    )
 ):
-    os.mkdir(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "test_data"))
+    os.mkdir(
+        os.path.join(
+            os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "test_data"
+        )
+    )
--- tests/test_sqlalchemy_migrate.py
+++ tests/test_sqlalchemy_migrate.py
@@ -7,13 +7,18 @@ import pytest
 @pytest.fixture
 def sql_migrate_object():
     from nldcsc.sql_migrations.flask_sql_migrate import FlaskSqlMigrate
+
     fsm = FlaskSqlMigrate(
-            app_ref="migrate_app",
-            cwd=os.path.join(os.path.dirname(os.path.realpath(__file__)), "applications"),
-        )
+        app_ref="migrate_app",
+        cwd=os.path.join(os.path.dirname(os.path.realpath(__file__)), "applications"),
+    )
     yield fsm
 
-    rmtree(os.path.join(os.path.dirname(os.path.realpath(__file__)), "applications", "migrations"))
+    rmtree(
+        os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "applications", "migrations"
+        )
+    )
     rmtree(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "instance"))
 
 
@@ -27,10 +32,21 @@ class TestSqlMigrator:
 
         sql_migrate_object.db_init()
 
-        assert os.path.exists(os.path.join(sql_migrate_object.current_dir, "migrations"))
+        assert os.path.exists(
+            os.path.join(sql_migrate_object.current_dir, "migrations")
+        )
 
         sql_migrate_object.db_migrate()
 
-        assert len(os.listdir(os.path.join(sql_migrate_object.current_dir, "migrations", "versions"))) != 0
+        assert (
+            len(
+                os.listdir(
+                    os.path.join(
+                        sql_migrate_object.current_dir, "migrations", "versions"
+                    )
+                )
+            )
+            != 0
+        )
 
         sql_migrate_object.db_update()
