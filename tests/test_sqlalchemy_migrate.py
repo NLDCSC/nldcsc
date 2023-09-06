de2b21d436bf6dad5fc3796d4ced12e6b5cfa31e
Paul Tikken <paul.tikken@gmail.com>
2023-09-06 18:43:07 +0000

    added test mocking file and directory checks

--- tests/test_sqlalchemy_migrate.py
+++ tests/test_sqlalchemy_migrate.py
@@ -1,15 +1,15 @@
 import logging
 import os.path
 from shutil import rmtree
-
+import mock
 import pytest
 
 from tests.helpers.capture_logging import catch_logs, records_to_tuples
+from nldcsc.sql_migrations.flask_sql_migrate import FlaskSqlMigrate
 
 
 @pytest.fixture
 def sql_migrate_object():
-    from nldcsc.sql_migrations.flask_sql_migrate import FlaskSqlMigrate
 
     fsm = FlaskSqlMigrate(
         app_ref="migrate_app",
@@ -34,7 +34,6 @@ def client(app):
 
 class TestSqlMigrator:
     def test_import(self):
-        from nldcsc.sql_migrations.flask_sql_migrate import FlaskSqlMigrate
 
         fsm = FlaskSqlMigrate()
 
@@ -56,6 +55,42 @@ class TestSqlMigrator:
                     "file in the current directory.",
                 )
             ]
+
+    @mock.patch("nldcsc.sql_migrations.flask_sql_migrate.os")
+    def test_class_init(self, fsm_os):
+        # set up the mock
+        fsm_os.path.exists.return_value = True
+        fsm_os.path.dirname.return_value = "/tmp/test"
+
+        fsm = FlaskSqlMigrate(app_ref="/tmp/test/app.py:app")
+
+        # check if FlaskSqlMigrate called os.path.dirname with the correct application path
+        fsm_os.path.dirname.assert_called_with("/tmp/test/app.py")
+
+        del fsm
+        # retest with no app name added to path
+        fsm = FlaskSqlMigrate(app_ref="/tmp/test/app.py")
+
+        fsm_os.path.dirname.assert_called_with("/tmp/test/app.py")
+
+        del fsm
+        # retest with no dirname returning ""
+        fsm_os.path.dirname.return_value = ""
+        with pytest.raises(ValueError):
+            fsm = FlaskSqlMigrate(app_ref="/tmp/test/app.py")
+            fsm_os.path.dirname.assert_called_with("/tmp/test/app.py")
+
+        # retest with os.path.exists returns False
+        fsm_os.path.exists.return_value = False
+
+        with pytest.raises(FileNotFoundError):
+            fsm = FlaskSqlMigrate(app_ref="/tmp/test/app.py")
+
+        with pytest.raises(AttributeError):
+            fsm = FlaskSqlMigrate(app_ref="tmp.test.app:app")
+
+        with pytest.raises(FileNotFoundError):
+            fsm = FlaskSqlMigrate(app_ref="tmp.test.app:app", cwd="/tmp/test")
 
     def test_migration_run(self, sql_migrate_object):
         sql_migrate_object.db_init()
