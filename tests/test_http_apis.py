1883e6d7d3f2bbe319699efa9d9546774516a948
Paul Tikken <paul.tikken@gmail.com>
2023-09-12 07:30:32 +0000

    paths tests altered

--- tests/test_http_apis.py
+++ tests/test_http_apis.py
@@ -9,10 +9,26 @@ class HttpApi(ApiBaseClass):
         super().__init__(baseurl, **kwargs)
 
     def get_dummy_endpoint(self):
-
         resource = "dummy"
 
         return self.call(self.methods.GET, resource=resource)
+
+    def post_str_dummy(self):
+        resource = "dummy"
+        data = "data"
+
+        return self.call(self.methods.POST, resource=resource, data=data)
+
+    def patch_dict_dummy(self):
+        resource = "dummy"
+        data = {"data": "data"}
+
+        return self.call(self.methods.PATCH, resource=resource, data=data)
+
+    def delete_dict_dummy(self):
+        resource = "dummy"
+
+        return self.call(self.methods.DELETE, resource=resource)
 
 
 @pytest.fixture
@@ -24,7 +40,12 @@ def http_api():
 
 @pytest.fixture
 def http_path_api():
-    ha = HttpApi(baseurl="http://localhost:8000", user_agent="HTTP_API", api_path="api", verify=True)
+    ha = HttpApi(
+        baseurl="http://localhost:8000",
+        user_agent="HTTP_API",
+        api_path="api",
+        verify=True,
+    )
 
     yield ha
 
@@ -68,7 +89,9 @@ class TestHttpApis:
 
         assert http_path_api.api_path == "api"
 
-        assert http_path_api._build_url("resource") == "http://localhost:8000/api/resource"
+        assert (
+            http_path_api._build_url("resource") == "http://localhost:8000/api/resource"
+        )
 
         assert http_api._build_url("resource") == "http://localhost:8000/resource"
 
@@ -76,3 +99,6 @@ class TestHttpApis:
 
         with pytest.raises(requests.ConnectionError):
             http_api.get_dummy_endpoint()
+            http_api.post_str_dummy()
+            http_api.patch_dict_dummy()
+            http_api.delete_dict_dummy()
