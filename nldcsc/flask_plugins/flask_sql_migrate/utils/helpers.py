42b9ac22b63c16220feccbb23231bf57115f9539
thomas-dolle <tce.dolle+def@gmail.com>
2024-08-27 08:47:57 +0200

    Copy of https://github.com/NLDCSC/sqlmigrate

---
+++ nldcsc/flask_plugins/flask_sql_migrate/utils/helpers.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+
+def timestamp_to_strf_string(timestamp: int) -> str:
+    """
+    Method that will take the provided timestamp and converts it into a RFC3339 date time string
+
+    Args:
+        timestamp: unix timestamp
+
+    Returns:
+        Date time string
+    """
+    value = datetime.utcfromtimestamp(timestamp)
+
+    return value.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def exclude_optional_dict(value):
+    return value is None or not value
