298189973dad4864b158dd84c8db03ec5f273d7d
Jerry de Boer <JF.d.Boer.01@mindef.nl>
2024-12-19 11:33:06 +0100

    Make whois record fields optional

--- nldcsc/plugins/whoisxml/objects/whois.py
+++ nldcsc/plugins/whoisxml/objects/whois.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
 
 from dataclasses_json import dataclass_json
 
@@ -9,11 +9,11 @@ from nldcsc.generic.times import timestringTOtimestamp
 @dataclass_json
 @dataclass
 class OrganisationDetails:
-    organization: str
-    state: str
-    country: str
-    countryCode: str
-    rawText: str
+    organization: Optional[str] = None
+    state: Optional[str] = None
+    country: Optional[str] = None
+    countryCode: Optional[str] = None
+    rawText: Optional[str] = None
 
 
 @dataclass_json
@@ -34,106 +34,125 @@ class TechnicalContact(OrganisationDetails): ...
 @dataclass_json
 @dataclass
 class NameServers:
-    rawText: str
-    hostNames: List[str]
-    ips: List[str]
+    rawText: Optional[str] = None
+    hostNames: Optional[List[str]] = None
+    ips: Optional[List[str]] = None
 
 
 @dataclass_json
 @dataclass
 class MetaData:
-    status: str
-    rawText: str
-    parseCode: int
-    header: str
-    strippedText: str
-    footer: str
-    audit: dict[str, str]
-    customField1Name: str
-    customField1Value: str
-    registrarName: str
-    registrarIANAID: str
-    createdDateNormalized: str
-    updatedDateNormalized: str
-    expiresDateNormalized: str
-    customField2Name: str
-    customField3Name: str
-    customField2Value: str
-    customField3Value: str
+    status: Optional[str] = None
+    rawText: Optional[str] = None
+    parseCode: Optional[int] = None
+    dataError: Optional[str] = None
+    header: Optional[str] = None
+    strippedText: Optional[str] = None
+    footer: Optional[str] = None
+    audit: Optional[dict[str, str]] = None
+    customField1Name: Optional[str] = None
+    customField1Value: Optional[str] = None
+    registrarName: Optional[str] = None
+    registrarIANAID: Optional[str] = None
+    createdDateNormalized: Optional[str] = None
+    updatedDateNormalized: Optional[str] = None
+    expiresDateNormalized: Optional[str] = None
+    customField2Name: Optional[str] = None
+    customField3Name: Optional[str] = None
+    customField2Value: Optional[str] = None
+    customField3Value: Optional[str] = None
 
 
 @dataclass_json
 @dataclass
 class RegistryData(MetaData):
-    domainName: str
-    nameServers: NameServers
-    whoisServer: str
-    createdDate: str | int = 0
-    updatedDate: str | int = 0
-    expiresDate: str | int = 0
+    domainName: Optional[str] = None
+    nameServers: Optional["NameServers"] = None
+    whoisServer: Optional[str] = None
+    createdDate: Optional[str] = None
+    updatedDate: Optional[str] = None
+    expiresDate: Optional[str] = None
 
     def __post_init__(self):
-        self.__setattr__(
-            "nameServers", NameServers(**self.__getattribute__("nameServers"))
-        )
+        if self.__getattribute__("nameServers") is not None:
+            self.__setattr__(
+                "nameServers", NameServers(**self.__getattribute__("nameServers"))
+            )
 
 
 @dataclass_json
 @dataclass
 class WhoisRecord(MetaData):
-    createdDate: str
-    updatedDate: str
-    expiresDate: str
-    registrant: Registrant
-    administrativeContact: AdministrativeContact
-    technicalContact: TechnicalContact
-    domainName: str
-    nameServers: NameServers
-    registryData: RegistryData
-    domainAvailability: str
-    contactEmail: str
-    domainNameExt: str
-    estimatedDomainAge: int
-    ips: List[str]
+    createdDate: Optional[str] = None
+    updatedDate: Optional[str] = None
+    expiresDate: Optional[str] = None
+    registrant: Optional["Registrant"] = None
+    administrativeContact: Optional["AdministrativeContact"] = None
+    technicalContact: Optional["TechnicalContact"] = None
+    domainName: Optional[str] = None
+    nameServers: Optional["NameServers"] = None
+    registryData: Optional["RegistryData"] = None
+    domainAvailability: Optional[str] = None
+    contactEmail: Optional[str] = None
+    domainNameExt: Optional[str] = None
+    estimatedDomainAge: Optional[int] = None
+    ips: Optional[List[str]] = None
 
     def __post_init__(self):
-        self.__setattr__(
-            "registrant", Registrant(**self.__getattribute__("registrant"))
-        )
-        self.__setattr__(
-            "administrativeContact",
-            AdministrativeContact(**self.__getattribute__("administrativeContact")),
-        )
-        self.__setattr__(
-            "technicalContact",
-            TechnicalContact(**self.__getattribute__("technicalContact")),
-        )
-        self.__setattr__(
-            "nameServers", NameServers(**self.__getattribute__("nameServers"))
-        )
-        self.__setattr__(
-            "registryData", RegistryData(**self.__getattribute__("registryData"))
-        )
+        if self.__getattribute__("registrant") is not None:
+            self.__setattr__(
+                "registrant", Registrant(**self.__getattribute__("registrant"))
+            )
+        if self.__getattribute__("administrativeContact") is not None:
+            self.__setattr__(
+                "administrativeContact",
+                AdministrativeContact(**self.__getattribute__("administrativeContact")),
+            )
+        if self.__getattribute__("technicalContact") is not None:
+            self.__setattr__(
+                "technicalContact",
+                TechnicalContact(**self.__getattribute__("technicalContact")),
+            )
+        if self.__getattribute__("nameServers") is not None:
+            self.__setattr__(
+                "nameServers", NameServers(**self.__getattribute__("nameServers"))
+            )
+        if self.__getattribute__("registryData") is not None:
+            self.__setattr__(
+                "registryData", RegistryData(**self.__getattribute__("registryData"))
+            )
 
     @property
     def domain_is_available(self):
-        if self.domainAvailability == "UNAVAILABLE":
+        if (
+            self.domainAvailability is not None
+            and self.domainAvailability == "UNAVAILABLE"
+        ):
             return False
         else:
             return True
 
     @property
-    def get_registrar(self) -> str:
+    def get_registrar(self) -> str | None:
         return self.registrarName
 
     @property
     def get_created_date(self) -> int:
-        return timestringTOtimestamp(self.registryData.createdDate)
+        if self.registryData.createdDate:
+            return timestringTOtimestamp(self.registryData.createdDate)
+        else:
+            return 0
 
     @property
     def get_updated_date(self) -> int:
-        return timestringTOtimestamp(self.registryData.updatedDate)
+        if self.registryData.updatedDate:
+            return timestringTOtimestamp(self.registryData.updatedDate)
+        else:
+            return 0
 
     @property
     def get_expires_date(self) -> int:
-        return timestringTOtimestamp(self.registryData.expiresDate)
+        if self.registryData.expiresDate:
+            return timestringTOtimestamp(self.registryData.expiresDate)
+        else:
+            return 0
