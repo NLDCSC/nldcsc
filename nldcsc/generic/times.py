5c4db1f35dcbe858ad9c9012d80f1b759017d14e
Jerry de Boer <JF.d.Boer.01@mindef.nl>
2024-12-19 14:49:15 +0100

    Add time parser

--- nldcsc/generic/times.py
+++ nldcsc/generic/times.py
@@ -25,6 +25,7 @@ def timestringTOtimestamp(timestring):
         "%Y-%m-%d %H:%M:%S UTC",
         "%Y-%m-%dT%H:%M:%S",
         "%Y-%m-%dT%H:%M:%SZ",
+        "%Y-%m-%dT%H:%M:%S%z",
         "%Y-%m-%dT%H:%M:%S.%f",
         "%Y-%m-%dT%H:%M:%S,%f",
         "%Y-%m-%dT%H:%M:%S.%fZ",
