257be44597fae750425ac78810e17e7806dd3cd7
Paul Tikken <paul.tikken@gmail.com>
2025-02-07 12:29:35 +0000

    added mandatory checking for string variables

--- nldcsc/generic/utils.py
+++ nldcsc/generic/utils.py
@@ -6,6 +6,8 @@ import secrets
 import string
 from json import JSONDecodeError
 
+__MANDATORY_VALUE__ = "<<MANDATORY_VALUE>>"
+
 
 def getenv_bool(name: str, default: str = "False"):
     raw = os.getenv(name, default).title()
@@ -52,8 +54,33 @@ def getenv_dict(name: str, default: dict = None):
     return default
 
 
-def str2bool(v):
-    return v.lower() in ("yes", "true", "1")
+def getenv_str(name: str, default: str = None, mandatory: bool = False):
+    raw = os.getenv(name, default)
+
+    if mandatory:
+        if raw == __MANDATORY_VALUE__:
+            raise ValueError(f"{name} is not set!")
+
+    return raw
+
+
+_true_set = {"yes", "true", "t", "y", "1"}
+_false_set = {"no", "false", "f", "n", "0"}
+
+
+def str2bool(value, raise_exc=True):
+    if isinstance(value, str):
+        value = value.lower()
+        if value in _true_set:
+            return True
+        if value in _false_set:
+            return False
+
+    if raise_exc:
+        raise ValueError(
+            f'Expected "{", ".join(_true_set | _false_set)}" -> received "{value}"'
+        )
+    return None
 
 
 def generate_random_password():
