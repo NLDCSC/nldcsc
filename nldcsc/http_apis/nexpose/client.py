94290ffbd2147fb298539acc1e2a53bd115ce9ac
thomas-dolle <tce.dolle+def@gmail.com>
2026-05-15 18:25:34 +0200

    wip nexpose

--- nldcsc/http_apis/nexpose/client.py
+++ nldcsc/http_apis/nexpose/client.py
@@ -1,17 +1,23 @@
 from functools import wraps
-from typing import Literal, Union
+from typing import Callable, Literal, Type, TypeVar, Union
 
 from nldcsc.http_apis.base_class.cached_base_class import CachedAPI
 from .objects import NexposeAssets
 
+T = TypeVar("T")
 
-def as_object(obj):
-    def wrapper(f):
+
+def as_object(
+    obj: Type[T], transform: Callable[..., T] = None
+) -> Callable[..., Callable[..., T]]:
+    def wrapper(f) -> Callable[..., T]:
         @wraps
-        def inner(*args, **kwargs):
+        def inner(*args, **kwargs) -> T:
             r = f(*args, **kwargs)
 
-            return obj(r)
+            if transform:
+                return transform(r)
+            return obj(**r)
 
         return inner
 
@@ -19,7 +25,7 @@ def as_object(obj):
 
 
 class NexposeClient(CachedAPI):
-    @as_object(NexposeAssets)
+    @as_object(NexposeAssets, NexposeAssets.from_dict)
     def get_assets(
         self,
         page: int = 0,
@@ -37,3 +43,22 @@ class NexposeClient(CachedAPI):
             params["sort"] = [f"{field},{order}" for field, order in sorting]
 
         return self.call(self.methods.GET, resource, params=params)
+
+    def iter_assets(
+        self,
+        offset: int = 0,
+        batch_size: int = 100,
+        sorting: list[tuple[str, Union[Literal["ASC"], Literal["DESC"]]]] = None,
+    ):
+        page = batch_size // offset
+
+        while True:
+            assets = self.get_assets(page, batch_size, sorting)
+
+            page += 1
+
+            if assets.page.number == assets.page.total_pages:
+                ...
+
+            if page * batch_size < offset:
+                pass
--- nldcsc/http_apis/nexpose/objects.py
+++ nldcsc/http_apis/nexpose/objects.py
@@ -1,5 +1,38 @@
 from dataclasses import dataclass
 
+from dataclasses_json import DataClassJsonMixin, LetterCase, config
+
+
+class NexposeDataClassConfig(DataClassJsonMixin):
+    dataclass_json_config = config(letter_case=LetterCase.CAMEL)["dataclasses_json"]
+
 
 @dataclass
-class NexposeAssets: ...
+class NexposeLinks(NexposeDataClassConfig):
+
+    deprecation: str
+    href: str
+    hreflang: str
+    media: str
+    rel: str
+    title: str
+    type: str
+
+
+@dataclass
+class NexposePage(NexposeDataClassConfig):
+    number: int
+    size: int
+    total_pages: int
+    total_resources: int
+
+
+@dataclass
+class NexposeResource(NexposeDataClassConfig): ...
+
+
+@dataclass
+class NexposeAssets(NexposeDataClassConfig):
+    links: list[NexposeLinks]
+    page: NexposePage
+    resources: list[NexposeResource]
