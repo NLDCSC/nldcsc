0b30b585b0b5a539a7acda803949d678e096740f
thomas-dolle <tce.dolle+def@gmail.com>
2026-05-20 14:20:03 +0200

    nexpose api to get assets and their vulnerabilities

--- nldcsc/http_apis/nexpose/client.py
+++ nldcsc/http_apis/nexpose/client.py
@@ -1,8 +1,17 @@
-from functools import wraps
-from typing import Callable, Literal, Type, TypeVar, Union
+from functools import partial, wraps
+from typing import Callable, Literal, Protocol, Type, TypeAlias, TypeVar, Union
 
 from nldcsc.http_apis.base_class.cached_base_class import CachedAPI
-from .objects import NexposeAssets
+from .objects import (
+    NexposeAssetVulnerabilities,
+    NexposeAssets,
+    NexposeLink,
+    NexposePage,
+    NexposeResource,
+    NexposeSortingDirection,
+    NexposeSearchMatch,
+    NexposeFilter,
+)
 
 T = TypeVar("T")
 
@@ -15,24 +24,37 @@ def as_object(
         def inner(*args, **kwargs) -> T:
             r = f(*args, **kwargs)
 
-            if transform:
-                return transform(r)
-            return obj(**r)
+            try:
+                if transform:
+                    return transform(r)
+                return obj(**r)
+            except Exception as e:
+                raise ValueError(
+                    f"Failed transforming response for {f.__name__} into {obj=} -> {e=}"
+                )
 
         return inner
 
     return wrapper
 
 
+Sorting: TypeAlias = list[
+    tuple[str, Union[Literal["ASC"], Literal["DESC"], NexposeSortingDirection]]
+]
+
+
+class NexposePageResponse(Protocol[T]):
+    links: list[NexposeLink]
+    page: NexposePage
+    resources: list[T]
+
+
+class NexposePageSource(Protocol[T]):
+    def __call__(self, page: int) -> NexposePageResponse[T]: ...
+
+
 class NexposeClient(CachedAPI):
-    @as_object(NexposeAssets, NexposeAssets.from_dict)
-    def get_assets(
-        self,
-        page: int = 0,
-        size: int = 10,
-        sorting: list[tuple[str, Union[Literal["ASC"], Literal["DESC"]]]] = None,
-    ):
-        resource = "assets"
+    def _pss_to_params(self, page: int = 0, size: int = 10, sorting: Sorting = None):
 
         params = {
             "page": page,
@@ -42,20 +64,20 @@ class NexposeClient(CachedAPI):
         if sorting:
             params["sort"] = [f"{field},{order}" for field, order in sorting]
 
-        return self.call(self.methods.GET, resource, params=params)
+        return params
 
-    def iter_assets(
+    def _iter_pages(
         self,
+        source: NexposePageSource[T],
         offset: int = 0,
         batch_size: int = 100,
-        sorting: list[tuple[str, Union[Literal["ASC"], Literal["DESC"]]]] = None,
     ):
         page = offset // batch_size
         skip = offset % batch_size
 
-        assets = self.get_assets(page, batch_size, sorting)
+        while True:
+            assets = source(page=page)
 
-        while assets.page.number < assets.page.total_pages:
             page += 1
 
             if skip:
@@ -63,3 +85,90 @@ class NexposeClient(CachedAPI):
                 skip = 0
 
             yield from assets.resources
+
+            if assets.page is None or page >= assets.page.total_pages:
+                break
+
+    @as_object(NexposeAssets, NexposeAssets.from_dict)
+    def get_assets(
+        self,
+        page: int = 0,
+        size: int = 10,
+        sorting: Sorting = None,
+        filters: list[NexposeFilter] = None,
+        match: Union[
+            Literal["any"], Literal["all"], NexposeSearchMatch
+        ] = NexposeSearchMatch.ALL,
+    ):
+        params = self._pss_to_params(page, size, sorting)
+
+        if filters:
+            resource = "assets/search"
+
+            return self.call(
+                self.methods.POST,
+                resource,
+                params=params,
+                json={"filters": filters, "match": match},
+            )
+
+        else:
+            resource = "assets"
+
+            return self.call(self.methods.GET, resource, params=params)
+
+    def iter_assets(
+        self,
+        offset: int = 0,
+        batch_size: int = 100,
+        sorting: Sorting = None,
+        filters: list[NexposeFilter] = None,
+        match: Union[
+            Literal["any"], Literal["all"], NexposeSearchMatch
+        ] = NexposeSearchMatch.ALL,
+    ):
+        yield from self._iter_pages(
+            partial(
+                self.get_assets,
+                size=batch_size,
+                sorting=sorting,
+                filters=filters,
+                match=match,
+            ),
+            offset,
+            batch_size,
+        )
+
+    def iter_asset_vulnerabilities(
+        self,
+        asset_id: int,
+        offset: int = 0,
+        batch_size: int = 100,
+        sorting: Sorting = None,
+    ):
+        yield from self._iter_pages(
+            partial(
+                self.get_asset_vulnerabilities,
+                asset_id=asset_id,
+                size=batch_size,
+                sorting=sorting,
+            ),
+            offset,
+            batch_size,
+        )
+
+    @as_object(NexposeAssetVulnerabilities, NexposeAssetVulnerabilities.from_dict)
+    def get_asset_vulnerabilities(
+        self, asset_id: int, page: int = 0, size: int = 10, sorting: Sorting = None
+    ):
+        resource = f"assets/{asset_id}/vulnerabilities"
+
+        return self.call(
+            self.methods.GET, resource, params=self._pss_to_params(page, size, sorting)
+        )
+
+    @as_object(NexposeResource, NexposeResource.from_dict)
+    def get_asset(self, asset_id: int):
+        resource = f"assets/{asset_id}"
+
+        return self.call(self.methods.GET, resource)
--- nldcsc/http_apis/nexpose/objects.py
+++ nldcsc/http_apis/nexpose/objects.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass, field
+from typing import Any, Optional, Type, TypedDict, NotRequired
 
 from dataclasses_json import DataClassJsonMixin, LetterCase, config, global_config
 from enum import StrEnum, Enum, auto
 
 
-def encode_as_string(e: Enum):
+def encode_as_string(e: Type[Enum]):
     """
     decorator to be used and encode the enum back to its str value
 
@@ -12,6 +13,81 @@ def encode_as_string(e: Enum):
         e (Enum): enum to register a string encoder for
     """
     global_config.encoders[e] = str
+
+    return e
+
+
+class NexposeFilterOperator(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name, start, count, last_values):
+        return name.lower().replace("_", "-")
+
+    IN = auto()
+    IS = auto()
+    IS_NOT = auto()
+    STARTS_WITH = auto()
+    ENDS_WITH = auto()
+    CONTAINS = auto()
+    DOES_NOT_CONTAIN = auto()
+    IS_LIKE = auto()
+    NOT_LIKE = auto()
+    ARE = auto()
+    IS_GREATER_THAN = auto()
+    IS_LESS_THAN = auto()
+    IS_APPLIED = auto()
+    IS_NOT_APPLIED = auto()
+    IS_EMPTY = auto()
+    IS_NOT_EMPTY = auto()
+    IN_RANGE = auto()
+    NOT_IN_RANGE = auto()
+    IS_ON_OR_BEFORE = auto()
+    IS_ON_OR_AFTER = auto()
+    IS_BETWEEN = auto()
+    IS_EARLIER_THAN = auto()
+    IS_WITHIN_THE_LAST = auto()
+    INCLUDES = auto()
+    DOES_NOT_INCLUDE = auto()
+
+
+class NexposeSearchMatch(StrEnum):
+    ANY = auto()
+    ALL = auto()
+
+
+class NexposeSortingDirection(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name, start, count, last_values):
+        return name
+
+    ASC = auto()
+    DESC = auto()
+
+
+@encode_as_string
+class NexposeVulnerabilityStatus(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name, start, count, last_values):
+        return name.lower().replace("_", "-")
+
+    VULNERABLE = auto()
+    INVULNERABLE = auto()
+    NO_RESULTS = auto()
+
+
+@encode_as_string
+class NexposeResultStatus(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name, start, count, last_values):
+        return name.lower().replace("_", "-")
+
+    UNKNOWN = auto()
+    NOT_VULNERABLE = auto()
+    VULNERABLE = auto()
+    VULNERABLE_VERSION = auto()
+    VULNERABLE_POTENTIAL = auto()
+    VULNERABLE_WITH_EXCEPTION_APPLIED = auto()
+    VULNERABLE_VERSION_WITH_EXCEPTION_APPLIED = auto()
+    VULNERABLE_POTENTIAL_WITH_EXCEPTION_APPLIED = auto()
 
 
 @encode_as_string
@@ -53,201 +129,275 @@ class NexposeHostNameSource(StrEnum):
     OTHER = auto()
 
 
+@encode_as_string
+class NexposeProtocol(StrEnum):
+    IP = auto()
+    ICMP = auto()
+    IGMP = auto()
+    GGP = auto()
+    TCP = auto()
+    PUP = auto()
+    UDP = auto()
+    IDP = auto()
+    ESP = auto()
+    ND = auto()
+    RAW = auto()
+
+
+@encode_as_string
+class NexposeLinkType(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name, start, count, last_values):
+        return name.lower().replace("_", "-")
+
+    SEED = auto()
+    ROBOTS = auto()
+    PDF = auto()
+    CSS = auto()
+    IMPLIED_DIR = auto()
+    RSS = auto()
+    REDIRECTION = auto()
+    SITEMAP = auto()
+    BACKUP = auto()
+    VCK_REWRITE = auto()
+    NON_REF_GUESS = auto()
+    SOFT_404 = auto()
+    HTML_REF = auto()
+    JS_STRING = auto()
+    QUERY_PARAM = auto()
+
+
+class NexposeFilter(TypedDict):
+    # https://help.rapid7.com/insightvm/en-us/api/index.html#section/Overview/Responses
+
+    field: str
+    operator: NexposeFilterOperator
+    value: NotRequired[Any]
+    lower: NotRequired[Any]
+    upper: NotRequired[Any]
+
+
 class NexposeDataClassConfig(DataClassJsonMixin):
     dataclass_json_config = config(letter_case=LetterCase.CAMEL)["dataclasses_json"]
 
 
 @dataclass
 class NexposeLink(NexposeDataClassConfig):
-    deprecation: str
-    href: str
-    hreflang: str
-    media: str
-    rel: str
-    title: str
-    type: str
+    deprecation: Optional[str] = None
+    href: Optional[str] = None
+    hreflang: Optional[str] = None
+    media: Optional[str] = None
+    rel: Optional[str] = None
+    title: Optional[str] = None
+    type: Optional[str] = None
 
 
 @dataclass
 class NexposePage(NexposeDataClassConfig):
-    number: int
-    size: int
-    total_pages: int
-    total_resources: int
+    number: Optional[int] = None
+    size: Optional[int] = None
+    total_pages: Optional[int] = None
+    total_resources: Optional[int] = None
 
 
 @dataclass
 class NexposeAddress(NexposeDataClassConfig):
-    ip: str
-    mac: str
+    ip: Optional[str] = None
+    mac: Optional[str] = None
 
 
 @dataclass
 class NexposeConfiguration(NexposeDataClassConfig):
-    name: str
-    value: str
+    name: Optional[str] = None
+    value: Optional[str] = None
 
 
 @dataclass
 class NexposeDatabase(NexposeDataClassConfig):
-    description: str
-    id: int
-    name: str
+    description: Optional[str] = None
+    id: Optional[int] = None
+    name: Optional[str] = None
 
 
 @dataclass
 class NexposeFile(NexposeDataClassConfig):
-    attributes: list[NexposeConfiguration]
-    name: str
-    size: int
-    type: NexposeFileType
+    attributes: list[NexposeConfiguration] = field(default_factory=list)
+    name: Optional[str] = None
+    size: Optional[int] = None
+    type: Optional[NexposeFileType] = None
 
 
 @dataclass
 class NexposeHistory(NexposeDataClassConfig):
-    date: str
-    description: str
-    scan_id: int
-    type: NexposeHistoryType
-    user: str
-    version: int
-    vulnerability_Exception_id: int
+    date: Optional[str] = None
+    description: Optional[str] = None
+    scan_id: Optional[int] = None
+    type: Optional[NexposeHistoryType] = None
+    user: Optional[str] = None
+    version: Optional[int] = None
+    vulnerability_Exception_id: Optional[int] = None
 
 
 @dataclass
 class NexposeID(NexposeDataClassConfig):
-    id: str
-    source: str
+    id: Optional[str] = None
+    source: Optional[str] = None
 
 
 @dataclass
 class NexposeCPE(NexposeDataClassConfig):
-    edition: str
-    language: str
-    other: str
-    part: str
-    product: str
-    sw_edition: str
-    target_hw: str
-    target_sw: str
-    update: str
-    vendor: str
-    version: str
-    v2_2: str = field(metadata=config(field_name="v2.2"))
-    v2_3: str = field(metadata=config(field_name="v2.3"))
+    edition: Optional[str] = None
+    language: Optional[str] = None
+    other: Optional[str] = None
+    part: Optional[str] = None
+    product: Optional[str] = None
+    sw_edition: Optional[str] = None
+    target_hw: Optional[str] = None
+    target_sw: Optional[str] = None
+    update: Optional[str] = None
+    vendor: Optional[str] = None
+    version: Optional[str] = None
+    v2_2: Optional[str] = field(metadata=config(field_name="v2.2"), default=None)
+    v2_3: Optional[str] = field(metadata=config(field_name="v2.3"), default=None)
 
 
 @dataclass
 class NexposeOSFingerprint(NexposeDataClassConfig):
-    architecture: str
-    configurations: list[NexposeConfiguration]
-    cpe: NexposeCPE
-    description: str
-    family: str
-    id: int
-    product: str
-    system_name: str
-    type: str
-    vendor: str
-    version: str
+    architecture: Optional[str] = None
+    configurations: Optional[list[NexposeConfiguration]] = field(default_factory=list)
+    cpe: Optional[NexposeCPE] = None
+    description: Optional[str] = None
+    family: Optional[str] = None
+    id: Optional[int] = None
+    product: Optional[str] = None
+    system_name: Optional[str] = None
+    type: Optional[str] = None
+    vendor: Optional[str] = None
+    version: Optional[str] = None
 
 
 @dataclass
 class NexposeAssetVulnerabilities(NexposeDataClassConfig):
-    critical: int
-    exploits: int
-    malware_kits: int
-    moderate: int
-    severe: int
-    total: int
+    critical: Optional[int] = None
+    exploits: Optional[int] = None
+    malware_kits: Optional[int] = None
+    moderate: Optional[int] = None
+    severe: Optional[int] = None
+    total: Optional[int] = None
 
 
 @dataclass
 class NexposeUser(NexposeDataClassConfig):
-    fullname: str
-    id: int
-    name: str
+    fullname: Optional[str] = None
+    id: Optional[int] = None
+    name: Optional[str] = None
 
 
 @dataclass
 class NexposeUserGroup(NexposeDataClassConfig):
-    id: int
-    name: str
+    id: Optional[int] = None
+    name: Optional[str] = None
 
 
 @dataclass
 class NexposeSoftware(NexposeDataClassConfig):
-    configurations: list[NexposeConfiguration]
-    cpe: NexposeCPE
-    description: str
-    family: str
-    id: int
-    product: str
-    type: str
-    vendor: str
-    version: str
+    configurations: Optional[list[NexposeConfiguration]] = field(default_factory=list)
+    cpe: Optional[NexposeCPE] = None
+    description: Optional[str] = None
+    family: Optional[str] = None
+    id: Optional[int] = None
+    product: Optional[str] = None
+    type: Optional[str] = None
+    vendor: Optional[str] = None
+    version: Optional[str] = None
 
 
 @dataclass
 class NexposeWebPage(NexposeDataClassConfig):
-    link_type: str
-    path: str
-    response: int
+    link_type: Optional[NexposeLinkType] = None
+    path: Optional[str] = None
+    response: Optional[int] = None
 
 
 @dataclass
 class NexposeWebApplication(NexposeDataClassConfig):
-    id: int
-    pages: list[NexposeWebPage]
-    root: str
-    virtual_host: str
+    id: Optional[int] = None
+    pages: Optional[list[NexposeWebPage]] = field(default_factory=list)
+    root: Optional[str] = None
+    virtual_host: Optional[str] = None
 
 
 @dataclass
 class NexposeService(NexposeDataClassConfig):
-    configurations: list[NexposeConfiguration]
-    databases: list[NexposeDatabase]
-    family: str
-    links: list[NexposeLink]
-    name: str
-    nic: str
-    port: int
-    product: str
-    protocol: str
-    user_groups: list[NexposeUserGroup]
-    users: list[NexposeUser]
-    vendor: str
-    version: str
-    web_applications: list[NexposeWebApplication]
+    configurations: Optional[list[NexposeConfiguration]] = field(default_factory=list)
+    databases: Optional[list[NexposeDatabase]] = field(default_factory=list)
+    family: Optional[str] = None
+    links: Optional[list[NexposeLink]] = field(default_factory=list)
+    name: Optional[str] = None
+    nic: Optional[str] = None
+    port: Optional[int] = None
+    product: Optional[str] = None
+    protocol: Optional[NexposeProtocol] = None
+    user_groups: Optional[list[NexposeUserGroup]] = field(default_factory=list)
+    users: Optional[list[NexposeUser]] = field(default_factory=list)
+    vendor: Optional[str] = None
+    version: Optional[str] = None
+    web_applications: Optional[list[NexposeWebApplication]] = field(
+        default_factory=list
+    )
 
 
 @dataclass
 class NexposeResource(NexposeDataClassConfig):
-    addresses: list[NexposeAddress]
-    assessed_for_policies: bool
-    assessed_for_vulnerabilities: bool
-    configurations: list[NexposeConfiguration]
-    databases: list[NexposeDatabase]
-    files: list[NexposeFile]
-    history: list[NexposeHistory]
-    host_name: str
-    host_names: list[str]
-    id: int
-    ids: list[NexposeID]
-    ip: str
+    addresses: Optional[list[NexposeAddress]] = field(default_factory=list)
+    assessed_for_policies: Optional[bool] = None
+    assessed_for_vulnerabilities: Optional[bool] = None
+    configurations: Optional[list[NexposeConfiguration]] = field(default_factory=list)
+    databases: Optional[list[NexposeDatabase]] = field(default_factory=list)
+    files: Optional[list[NexposeFile]] = field(default_factory=list)
+    history: Optional[list[NexposeHistory]] = field(default_factory=list)
+    host_name: Optional[str] = None
+    host_names: Optional[list[str]] = field(default_factory=list)
+    id: Optional[int] = None
+    ids: Optional[list[NexposeID]] = field(default_factory=list)
+    ip: Optional[str] = None
+    links: Optional[list[NexposeLink]] = field(default_factory=list)
+    mac: Optional[str] = None
+    os: Optional[str] = None
+    os_certainty: Optional[str] = None
+    os_fingerprint: Optional[NexposeOSFingerprint] = None
+    raw_risk_score: Optional[float] = None
+    risk_score: Optional[float] = None
+    services: Optional[list[NexposeService]] = field(default_factory=list)
+    software: Optional[list[NexposeSoftware]] = field(default_factory=list)
+    type: Optional[NexposeAssetType] = None
+    user_groups: Optional[list[NexposeUserGroup]] = field(default_factory=list)
+    users: Optional[list[NexposeUser]] = field(default_factory=list)
+    vulnerabilities: Optional[NexposeAssetVulnerabilities] = None
+
+
+@dataclass
+class NexposeResult(NexposeDataClassConfig):
+    check_id: Optional[str] = None
+    exceptions: Optional[list[int]] = field(default_factory=list)
+    key: Optional[str] = None
+    links: Optional[list[NexposeLink]] = field(default_factory=list)
+    port: Optional[int] = None
+    proof: Optional[str] = None
+    protocol: Optional[NexposeProtocol] = None
+    reintroduced: Optional[str] = None
+    since: Optional[str] = None
+    status: Optional[NexposeResultStatus] = None
+
+
+@dataclass
+class NexposeVulnerability(NexposeDataClassConfig):
+    id: str
+    instances: int
     links: list[NexposeLink]
-    mac: str
-    os: str
-    os_certainty: str
-    os_fingerprint: NexposeOSFingerprint
-    raw_risk_score: float
-    risk_score: float
-    services: list[NexposeService]
-    software: list[NexposeSoftware]
-    type: NexposeAssetType
-    user_groups: list[NexposeUserGroup]
-    users: list[NexposeUser]
-    vulnerabilities: NexposeAssetVulnerabilities
+    status: NexposeVulnerabilityStatus
+    results: Optional[list[NexposeResult]] = field(default_factory=list)
+    since: Optional[str] = None
 
 
 @dataclass
@@ -255,3 +405,10 @@ class NexposeAssets(NexposeDataClassConfig):
     links: list[NexposeLink]
     page: NexposePage
     resources: list[NexposeResource]
+
+
+@dataclass
+class NexposeAssetVulnerabilities(NexposeDataClassConfig):
+    links: list[NexposeLink]
+    page: NexposePage
+    resources: list[NexposeVulnerability]
