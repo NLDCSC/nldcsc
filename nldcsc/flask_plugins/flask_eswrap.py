eb07675cb41e89fa3e1a45f8d624288fb3974141
thomas-dolle <tce.dolle+def@gmail.com>
2026-06-18 14:26:54 +0200

    add compat thing for elastic

--- nldcsc/flask_plugins/flask_eswrap.py
+++ nldcsc/flask_plugins/flask_eswrap.py
@@ -1,8 +1,15 @@
+from functools import wraps
 import os
+import re
 
 from eswrap import EsWrap
 
 from nldcsc.generic.utils import getenv_list, getenv_dict
+
+_COMPAT_MIMETYPE_RE = re.compile(
+    r"application/(?:vnd\.elasticsearch\+)?(json|x-ndjson|vnd\.mapbox-vector-tile)"
+    r"(?:\s*;\s*compatible-with=\d+)?"
+)
 
 
 class FlaskEsWrap(object):
@@ -21,6 +28,7 @@ class FlaskEsWrap(object):
         self.elastic_port = int(os.getenv("ELASTIC_PORT", 9200))
         self.elastic_scheme = os.getenv("ELASTIC_SCHEME", "http")
 
+        self.elastic_compat = os.getenv("ELASTIC_COMPAT_VERSION", None)
         # Or as a list[str] | list[dict], takes preference over Single elastic connection details settings
         # When using this option Authentication should be a part of the connection strings,
         # e.g. http://elastic:changeme@localhost:9200/
@@ -31,6 +39,30 @@ class FlaskEsWrap(object):
 
         if app is not None or init_standalone:
             self.init_app(app)
+
+    def apply_compat_with(self, client: EsWrap):
+        transport = client.es_client.transport
+
+        if "perform_request" in transport.__dict__:
+            return client
+
+        sub = rf"application/vnd.elasticsearch+\g<1>; compatible-with={self.elastic_compat}"
+        original_perform_request = transport.perform_request
+
+        @wraps(original_perform_request)
+        def perform_request(*args, **kwargs):  # type: ignore[no-untyped-def]
+            headers = kwargs.get("headers")
+            if not headers:
+                return original_perform_request(*args, **kwargs)
+
+            merged = dict(headers)
+            for key in list(merged):
+                if key.lower() in ("accept", "content-type") and merged[key]:
+                    merged[key] = _COMPAT_MIMETYPE_RE.sub(sub, merged[key])
+            kwargs["headers"] = merged
+            return original_perform_request(*args, **kwargs)
+
+        setattr(transport, "perform_request", perform_request)
 
     def init_app(self, app, **kwargs):
         self.kwargs.update(kwargs)
@@ -72,6 +104,9 @@ class FlaskEsWrap(object):
 
         if app is not None:
             app.flask_eswrap = self
+
+        if self.elastic_compat:
+            self.apply_compat_with(self._es)
 
     @property
     def es(self) -> EsWrap:
